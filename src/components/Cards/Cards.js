43ce7da891da6eb99efa9225e450145caa9c8f36
Connor Tangney <Couch3ater@gmail.com>
2019-01-23 14:52:13 -0500

    added some documentation to Card component

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,29 +1,99 @@
+/*
+
+	card: {
+		a basic, material design card component;
+		default card component will display from the top down: header -> image -> body -> footer;
+		check usage for variable layouts;
+	}
+
+	props: {
+		cardHeader: {
+			optional prop;
+			display a card-header row when prop is not undefined;
+		}
+		imgSrc: {
+			optional prop;
+			display an image card when not undefined;
+		}
+		altTxt: {
+			required when imgSrc prop is not undefined;
+		}
+		cardBody: {
+			optional prop;
+			display a card-body row when prop is not undefined;
+		}
+		cardFooter: {
+			optional prop;
+			display a card-footer row when prop is not undefined;
+		}
+		classes: {
+			optional prop;
+			any additional classes that should be applied to the card component;
+		}
+	}
+
+	usage: {
+		default card component will display from the top down: header -> image -> body -> footer;
+		pass additional class through the classes prop to modify the layout: {
+			1-column layout: {
+				no-header: image -> body -> footer;
+				no-img: header -> body -> footer;
+				no-footer: header -> image -> body;
+
+				img-top-header: image -> header -> body -> footer;
+				img-top-no-header: image -> body -> footer;
+				img-top-no-footer: image -> header -> body;
+			}
+			2-column layouts: {
+				img-left: {
+					left: 100% height image;
+					right: header -> body -> footer;
+				}
+				img-left-no-footer: {
+					left: 100% height image;
+					right: header -> body;
+				}
+
+				img-right: {
+					left: header -> body -> footer;
+					right: 100% height image;
+				}
+				img-right-no-footer: {
+					left: header -> body;
+					right: 100% height image;
+				}
+			}
+		}
+	}
+
+*/
+
 import React from 'react';
 import './Cards.css';
 
 const Card = (e) => {
-	let getHeader = () => {
+	const getHeader = () => {
 		return(
 			<div className='card-header' key='card-header'>
 				<h1>{e.cardHeader}</h1>
 			</div>
 		);
 	}
-	let getImg = () => {
+	const getImg = () => {
 		return(
 			<div className='card-img' key='card-img'>
 				<img src={e.imgSrc} alt={e.altTxt} width='100%' />
 			</div>
 		)
 	}
-	let getBody = () => {
+	const getBody = () => {
 		return(
 			<div className='card-body' key='card-body'>
 				{e.cardBody}
 			</div>
 		)
 	}
-	let getFooter = () => {
+	const getFooter = () => {
 		return(
 			<div className='card-footer' key='card-footer'>
 				{e.cardFooter}
@@ -32,7 +102,7 @@ const Card = (e) => {
 	}
 	const getClasses = () => {
 		let output = ''
-		e.classes ? output=e.classes + ' ' : output='';
+		e.classes ? output=' ' + e.classes : output='';
 		return output;
 	}
 
@@ -44,7 +114,7 @@ const Card = (e) => {
 
 	return (
 		/* CARD */
-		<div className={getClasses() + 'card'}>
+		<div className={'card' + getClasses()}>
 			{content}
 		</div>
 	);
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,3 +1,11 @@
+/*
+
+	home{
+		home page view;
+	}
+
+*/
+
 import React from 'react';
 
 import { Card } from '../../components/Cards/Cards';
@@ -9,6 +17,7 @@ class Home extends React.Component {
     return (
     	<div className='columns' id='card-container'>
     		<div className='column col-xs-12 col-sm-10 col-md-8 col-lg-6 col-6 col-mx-auto'>
+    			{/* A CARD COMPONENT */}
     			<Card 
     				classes='text-center'
 		      	cardHeader={'Hey there!'}
@@ -18,9 +27,7 @@ class Home extends React.Component {
 		      			<p className='text-large'>Welcome to my little corner of the Internet. I'm in the middle of some construction here, so check back soon!</p>
 		      		</div>
 		      	} 
-		      	cardFooter={''}
 		    	/>
-		    	<img id='logo' src={require('../../img/smile.png')} alt='Spinny Smile Logo' className='img-responsive p-centered' />
     		</div>
     	</div>
     );
