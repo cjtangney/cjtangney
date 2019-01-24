75cdbb2adb04c504563256e31bd05e0d9391867e
cjtangney <cjtangney@gmail.com>
2019-01-24 12:26:07 -0500

    card component added alternate layouts (img-top, img-left, img-right) added prop validation adjusted css properties

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,123 +1,153 @@
-/*
+/* component info
 
 	card: {
-		a basic, material design card component;
+		a basic card component;
 		default card component will display from the top down: header -> image -> body -> footer;
-		check usage for variable layouts;
-	}
-
-	props: {
-		cardHeader: {
-			optional prop;
-			display a card-header row when prop is not undefined;
-		}
-		imgSrc: {
-			optional prop;
-			display an image card when not undefined;
-		}
-		altTxt: {
-			required when imgSrc prop is not undefined;
-		}
-		cardBody: {
-			optional prop;
-			display a card-body row when prop is not undefined;
-		}
-		cardFooter: {
-			optional prop;
-			display a card-footer row when prop is not undefined;
-		}
-		classes: {
-			optional prop;
-			any additional classes that should be applied to the card component;
-		}
+		check usage for alternate layouts;
 	}
 
 	usage: {
-		default card component will display from the top down: header -> image -> body -> footer;
+		card contents stored in array;
+		default card component will display contents from the top down: header -> image -> body -> footer;
 		pass additional class through the classes prop to modify the layout: {
 			1-column layout: {
-				no-header: image -> body -> footer;
-				no-img: header -> body -> footer;
-				no-footer: header -> image -> body;
-
-				img-top-header: image -> header -> body -> footer;
-				img-top-no-header: image -> body -> footer;
-				img-top-no-footer: image -> header -> body;
+		  	img-top: {
+		  		image -> header -> body -> footer;
+		  	}
 			}
 			2-column layouts: {
-				img-left: {
+		    img-left: {
 					left: 100% height image;
 					right: header -> body -> footer;
 				}
-				img-left-no-footer: {
-					left: 100% height image;
-					right: header -> body;
-				}
-
-				img-right: {
+		    img-right: {
 					left: header -> body -> footer;
-					right: 100% height image;
-				}
-				img-right-no-footer: {
-					left: header -> body;
 					right: 100% height image;
 				}
 			}
 		}
 	}
-
 */
 
 import React from 'react';
+import propTypes from 'prop-types';
 import './Cards.css';
 
-const Card = (e) => {
+const Card = (props) => {
 	const getHeader = () => {
 		return(
 			<div className='card-header' key='card-header'>
-				<h1>{e.cardHeader}</h1>
+				<h1>{props.cardHeader}</h1>
 			</div>
 		);
 	}
 	const getImg = () => {
 		return(
 			<div className='card-img' key='card-img'>
-				<img src={e.imgSrc} alt={e.altTxt} width='100%' />
+				<img className='img-responsive' src={props.imgSrc} alt={props.altTxt} width='100%' />
 			</div>
 		)
 	}
 	const getBody = () => {
 		return(
 			<div className='card-body' key='card-body'>
-				{e.cardBody}
+				{props.cardBody}
 			</div>
 		)
 	}
 	const getFooter = () => {
 		return(
 			<div className='card-footer' key='card-footer'>
-				{e.cardFooter}
+				{props.cardFooter}
 			</div>
 		)
 	}
-	const getClasses = () => {
-		let output = ''
-		e.classes ? output=' ' + e.classes : output='';
-		return output;
-	}
+	const getCard = () => {
+		let content = [];
+		let classes = '';
+		if(props.classes){classes = 'card ' + props.classes;}
+		else{classes = 'card';}
 
-	let content = [];
-	if(e.cardHeader)content.push(getHeader());
-	if(e.imgSrc)content.push(getImg());
-	if(e.cardBody)content.push(getBody());
-	if(e.cardFooter)content.push(getFooter());
+		//store card contents
+		if(props.cardHeader)content.push(getHeader());
+		if(props.imgSrc)content.push(getImg());
+		if(props.cardBody)content.push(getBody());
+		if(props.cardFooter)content.push(getFooter());
+
+		//pull imgContent from card contents, move it to front
+		if(classes.includes('img-top')){
+			let imgContent = content.find(item => item.key === 'card-img');
+			let tempContent = content.filter(item => item.key !== 'card-img');
+			tempContent.unshift(imgContent);
+			content = tempContent;
+		}
+
+		//pull imgContent from card contents, separate into two columns
+		if(classes.includes('img-left')){
+			let imgContent = content.find(item => item.key === 'card-img');
+			content = content.filter(item => item.key !== 'card-img');
+
+			return(
+				<div className={classes}>
+					<div className='columns'>
+						<div className='column col-md-12 col-lg-7 col-8'>
+							{imgContent}
+						</div>
+						<div className='column col-md-12 col-lg-5 col-4'>
+							{content}
+						</div>
+					</div>
+				</div>
+			)
+		}
+
+		//pull imgContent from card contents, separate into two columns
+		if(classes.includes('img-right')){
+			let imgContent = content.find(item => item.key === 'card-img');
+			content = content.filter(item => item.key !== 'card-img');
+
+			return(
+				<div className={classes}>
+					<div className='columns'>
+						<div className='column col-md-12 col-lg-5 col-4'>
+							{content}
+						</div>
+						<div className='column col-md-12 col-lg-7 col-8'>
+							{imgContent}
+						</div>
+					</div>
+				</div>
+			)
+		}
+
+		return(
+			<div className={classes}>
+				{content}
+			</div>
+		);
+	}
 
 	return (
 		/* CARD */
-		<div className={'card' + getClasses()}>
-			{content}
-		</div>
+		getCard()
 	);
+}
+
+/* prop info
+*/
+Card.propTypes = {
+	cardHeader: propTypes.oneOfType([propTypes.string, propTypes.object]),
+	cardBody: propTypes.oneOfType([propTypes.string, propTypes.object]),
+	cardFooter: propTypes.oneOfType([propTypes.string, propTypes.object]),
+	imgSrc: propTypes.string,
+	//altTxt is only required if an imgSrc prop exists
+	altTxt: (function(props, propName, componentName){
+		if(props['imgSrc'] && !props[propName]){
+			return new Error(`An 'altTxt' prop must be supplied when an 'imgSrc' prop exists. Component: ${componentName}.`);
+		}else if(props['imgSrc'] && props[propName] && typeof(props[propName]) != 'string'){
+			return new Error(`'altTxt' only accepts a prop type of 'string'. Component: ${componentName}. Prop type: '${typeof(props[propName])}'.`);
+		}
+	}),
 }
 
 export { Card };
--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -1,9 +1,4 @@
-/*
-
-	home{
-		home page view;
-	}
-
+/* view info
 */
 
 import React from 'react';
@@ -15,21 +10,27 @@ import './Home.css';
 class Home extends React.Component {
   render() {
     return (
-    	<div className='columns' id='card-container'>
-    		<div className='column col-xs-12 col-sm-10 col-md-8 col-lg-6 col-6 col-mx-auto'>
-    			{/* A CARD COMPONENT */}
-    			<Card 
-    				classes='text-center'
-		      	cardHeader={'Hey there!'}
-		      	cardBody={
-		      		<div>
-		      			<p className='text-large'>My name is Connor!</p>
-		      			<p className='text-large'>Welcome to my little corner of the Internet. I'm in the middle of some construction here, so check back soon!</p>
-		      		</div>
-		      	} 
-		    	/>
-    		</div>
-    	</div>
+      <div className='columns'>
+        <div className='column col-xs-12 col-sm-10 col-md-8 col-lg-7 col-7 col-mx-auto' id='card-container'>
+          {/* card component */}
+          <Card 
+            classes='text-center img-top'
+            imgSrc='/img/puppy.jpg' 
+            altTxt='A puppy'  
+            cardBody={
+              <div>
+                <h1>Hey there!</h1>
+                <p className='text-large'>My name is Connor.</p>
+                <p className='text-large'>Welcome to my little corner of the Internet. I'm in the middle of some construction here, so check back soon.</p>
+                <p className='text-large'>In the mean time, why not check out these puppies and kittens? Who doesn't love puppies and kittens?!</p>
+              </div>
+            } 
+            cardFooter={
+              <button className='btn puppies-and-kittens'>Heck yea baby animals!</button>
+            }
+          />
+        </div>
+      </div>
     );
   }
 }
