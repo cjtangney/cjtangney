c2fcb960fe270342ea94a4943665f6e2270c1abb
cjtangney <cjtangney@gmail.com>
2019-01-28 21:45:16 -0500

    updates to resmue data and layout

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -6,7 +6,7 @@ import { Link } from 'react-router-dom';
 import { Card } from '../../components/Cards/Cards';
 //import { Modal } from '../../components/Modal/Modal';
 //import { Gallery } from '../../components/Gallery/Gallery';
-//import './Home.css';
+import './Resume.css';
 
 //const PUBLIC = process.env.PUBLIC_URL;
 
@@ -25,20 +25,34 @@ class Resume extends React.Component {
     return (
       <div className='card-container'>
         {console.log(this.state.data)}
-        <div className='columns'>
-          <div className='column col-xs-12 col-sm-10 col-md-8 col-lg-7 col-7 col-mx-auto'>
-            <Card 
-              classes='text-center'
-              cardHeader='Hey again!' 
+        <Card 
+          classes='text-center resume-header'
+          cardHeader='Hey again!' 
+          cardBody={
+            <div className='container'>
+              <p className='text-large'>If you've landed on this page, you're probably interested in learning a little more about my professional history. If you have any questions, don't hesitate to reach out to me at {this.state.data.basics.email}!</p>
+            </div>
+          }
+          cardFooter={
+            <Link to='/'>
+              <button className='btn'>Take me home!</button>
+            </Link>
+          }
+        />
+        <div className='container text-center'>
+          <div className='columns'>
+            <Card
+              classes='column col-sm-12 col-md-10 col-10 col-mx-auto'
+              cardHeader='Core Competencies'
               cardBody={
-                <div className='container'>
-                  <p className='text-large'>If you've landed on this page, you're probably interested in learning a little more about my professional history. If you have any questions, don't hesitate to reach out to me at {this.state.data.basics.email}!</p>
+                <div className='columns'>
+                  {this.state.data.competencies.map(compentency => 
+                    <div className='panel col-sm-11 col-md-3 col-3 col-mx-auto text-left resume-panel'>
+                      <p className='text-large'>{compentency.name}</p>
+                      <p>{compentency.summary}</p>
+                    </div>
+                  )}
                 </div>
-              }
-              cardFooter={
-                <Link to='/'>
-                  <button className='btn'>Take me home!</button>
-                </Link>
               }
             />
           </div>
